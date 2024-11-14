3977984b6760505dad39c357c8297df560d07572
Anuada <ericsonanuada18@gmail.com>
2024-11-14 09:39:16 +0800

    Updadated Code

--- ventory/logic/delete_pod_items.php
+++ ventory/logic/delete_pod_items.php
@@ -3,11 +3,12 @@ require_once "../util/dbhelper.php";
 
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
+$purchase_order_id = isset($_GET["purchase_order_id"]) ? $_GET["purchase_order_id"] : null;
 
     $db = new DbHelper();
     $result = $db->deletePod_Items($id); 
-    header("Location: ../pod.php?message=" . urlencode($result));
-    exit();
+    header("Location: ../page/view_details_purchase_order.php?message=" . urlencode($result) . "&purchase_order_id=" . urlencode($purchase_order_id));
+        exit();
 } else {
     header("Location: ./index.php?message=" . urlencode("Invalid ID provided for deletion."));
     exit();
